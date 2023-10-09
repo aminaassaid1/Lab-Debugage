c0b6a72cdc1f866905bca80382f3a7f704edf71e
amina <aminaassaid123@gmail.com>
2023-10-09 23:55:12 +0100

    done

--- index.php
+++ index.php
@@ -1,18 +1,36 @@
 <?php
-
 try {
     // Database connection parameters
-    $serverName = "localhost";
+    $serverName = "localhost:3306";
     $username = "root";
-    $password = "Andromed@123";
+    $password = "";
     $dbname = "sprint1";
 
     // Create a PDO instance with error handling
-    $conn = new PDO("mysql:host=$serverName;dbname=$dbname;charset=utf8", $username, $password);
+    $dsn = "mysql:host=$serverName;dbname=$dbname";
+    $conn = new PDO($dsn, $username, $password);
 
     // Set PDO error mode to exceptions
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "connected";
+    echo "Connected successfully";
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
+
+$sql = "SELECT * FROM personne"; // Corrected table name
+$result = $conn->query($sql);
+
+if ($result->rowCount() > 0) {
+    // Output data of each row
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        echo "ID: " . $row["id"] . "<br>";
+        echo "Name: " . $row["nom"] . "<br>";
+        echo "Surname: " . $row["prenom"] . "<br>";
+        echo "CNE: " . $row["CNE"] . "<br>";
+        echo "<hr>";
+    }
+} else {
+    echo "No personne found.";
+}
+
+?>
